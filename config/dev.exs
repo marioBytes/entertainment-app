64e7de1a61b4e7c6efa26e638bbcf5c0f72285af
marioBytes <mario.bytes92@gmail.com>
2022-11-17 16:28:40 -0700

    Install TailwindCSS

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :entertainment, EntertainmentWeb.Endpoint,
   secret_key_base: "NcWLRq6oJVWxR37mHfUcUOoi9HN63/ruE03c4zPYqefGjH3BZQZ1sowLINdkqOiX",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support

721bd5267997f4c1e0bbecab1915209f62ececb3
marioBytes <mario.bytes92@gmail.com>
2022-11-17 16:26:45 -0700

    Create component to handle bookmarking

--- lib/entertainment_web/live/video_live/index.ex
+++ lib/entertainment_web/live/video_live/index.ex
@@ -2,28 +2,17 @@ defmodule EntertainmentWeb.VideoLive.Index do
   use EntertainmentWeb, :live_view
 
   alias Entertainment.Media
-  alias Entertainment.Media.Video
+
+  alias EntertainmentWeb.VideoLive.Video.VideoComponent
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :videos, list_videos())}
+    {:ok, assign(socket, :videos, list_videos(socket.assigns.current_user))}
   end
 
   @impl true
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Video")
-    |> assign(:video, Media.get_video!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Video")
-    |> assign(:video, %Video{})
   end
 
   defp apply_action(socket, :index, _params) do
@@ -32,15 +21,7 @@ defmodule EntertainmentWeb.VideoLive.Index do
     |> assign(:video, nil)
   end
 
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    video = Media.get_video!(id)
-    {:ok, _} = Media.delete_video(video)
-
-    {:noreply, assign(socket, :videos, list_videos())}
-  end
-
-  defp list_videos do
+  defp list_videos(%{bookmarked_videos: bookmarked_videos}) do
     Media.list_videos()
   end
 end
---
+++ lib/entertainment_web/live/video_live/video/video_component.ex
@@ -0,0 +1,75 @@
+defmodule EntertainmentWeb.VideoLive.Video.VideoComponent do
+  use EntertainmentWeb, :live_component
+
+  alias Entertainment.Accounts
+  alias Entertainment.Accounts.BookmarkedUserVideo
+  alias Entertainment.Media
+
+  def update(assigns, socket) do
+    {:ok, socket |> assign(assigns) |> assign_video_is_bookmarked?(assigns.video.id)}
+  end
+
+  @impl true
+  def handle_event(
+        "add-video-bookmark",
+        _,
+        %{assigns: %{video: video, current_user: user}} = socket
+      ) do
+    case Accounts.bookmark_video(%{user_id: user.id, video_id: video.id}) do
+      {:ok, _} ->
+        {:noreply, assign_video_is_bookmarked?(socket, true)}
+
+      {:error, %Ecto.Changeset{}} ->
+        {:noreply, put_flash(socket, :error, "There was an error when bookmarking this video")}
+    end
+  end
+
+  @impl true
+  def handle_event(
+        "remove-video-bookmark",
+        _,
+        %{assigns: %{video: video, current_user: user}} = socket
+      ) do
+    bookmarked_user_video = Media.get_bookmarked_user_video(user.id, video.id)
+
+    case bookmarked_user_video do
+      %BookmarkedUserVideo{} = buv ->
+        case Accounts.remove_bookmarked_video(buv) do
+          {:ok, _} ->
+            {:noreply, assign_video_is_bookmarked?(socket, false)}
+
+          {:error, %Ecto.Changeset{}} ->
+            {:noreply,
+             put_flash(socket, :error, "There was an error when removing this bookmarked video")}
+        end
+
+      nil ->
+        socket =
+          socket |> put_flash(:error, "There was an error when removing this bookmarked video")
+
+        {:noreply, socket}
+    end
+  end
+
+  defp assign_video_is_bookmarked?(socket, value) when is_integer(value) do
+    bookmarked? = video_is_bookmarked?(socket, value)
+
+    assign(socket, :video_is_bookmarked, bookmarked?)
+  end
+
+  defp assign_video_is_bookmarked?(socket, value) when is_boolean(value) do
+    assign(socket, :video_is_bookmarked, value)
+  end
+
+  defp video_is_bookmarked?(%{assigns: %{current_user: %{bookmarked_videos: videos}}}, video_id) do
+    video_ids = map_bookmarked_video_ids(videos)
+
+    video_id in video_ids
+  end
+
+  defp map_bookmarked_video_ids(bookmarked_videos) do
+    Enum.map(bookmarked_videos, fn bookmarked_video ->
+      bookmarked_video.video_id
+    end)
+  end
+end
